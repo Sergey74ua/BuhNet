9cb6aeca01861d3c9b5ef6869c1cdaadd8261d13
Sergey74ua <Sergey74ua@gmail.com>
2021-04-15 22:35:32 +0300

    migrate user-company

---
+++ database/migrations/2021_04_15_155743_create_user_companies_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserCompaniesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_company', function (Blueprint $table) {
+            $table->bigInteger('id_user', 20)->index();
+            $table->bigInteger('id_company', 20)->index();
+            $table->date('validity')->nullable();
+            $table->bigInteger('director', 20)->nullable();
+            $table->string('specialty')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_company');
+    }
+}
