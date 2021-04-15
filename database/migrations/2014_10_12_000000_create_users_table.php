a2ebc660ea09347ace54984f9d8a23883ebaf291
Sergey74ua <Sergey74ua@gmail.com>
2021-04-15 22:24:57 +0300

    Update 2014_10_12_000000_create_users_table.php

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -8,18 +8,24 @@ class CreateUsersTable extends Migration
 {
     /**
      * Run the migrations.
-     *
+     * 
      * @return void
      */
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('name')->index();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
+            $table->boolean('theme_colors')->default(false);
+            $table->char('language', 2)->default('ru');
+            $table->char('rank', 7)->default('user');
+            $table->char('tariff', 7)->nullable();
+            $table->double('deposit', 12, 2)->nullable();
+            $table->string('avatar')->nullable();
             $table->timestamps();
         });
     }
